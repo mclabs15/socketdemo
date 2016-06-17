55ca8a37b2c6ff2caa9b1e0e2606a61f3d12e996
mclabs15 <mclabs15@gmail.com>
2016-06-17 11:23:58 -0700

    initial

---
+++ client/game.js
@@ -0,0 +1,105 @@
+window.onload = function() {
+
+var nodes = [];
+
+var socket = io();
+
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+}
+
+fillCircle = function(cx,cy,radius) {
+
+ctx.beginPath();
+ctx.arc(cx, cy, radius, 0, 2 * Math.PI, false);
+ctx.fill();
+
+};
+line = function(x1,y1,x2,y2) {
+ctx.beginPath();
+ctx.moveTo(x1,y1);
+ctx.lineTo(x2,y2);
+ctx.stroke();
+
+};
+
+
+
+var canvas = document.getElementById("main");
+var ctx = canvas.getContext("2d");
+
+
+pointDistance = function(x1,y1,x2,y2) {
+	return Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2));
+}
+
+
+setInterval(function(){
+
+window.w = window.innerWidth;
+window.h = window.innerHeight;
+
+canvas.setAttribute("width",window.w);
+canvas.setAttribute("height",window.h);
+
+ctx.fillStyle = "#000";
+ctx.fillRect(0,0,window.w,window.h);
+
+for(var i = 0; i < nodes.length; i++) {
+	for(var j = 0; j < nodes.length; j++) {
+		if(pointDistance(nodes[i].x,nodes[i].y,nodes[j].x,nodes[j].y) <= 150) {
+			ctx.lineWidth = "3px";
+			ctx.strokeStyle = "#072";
+			line(nodes[i].x,nodes[i].y,nodes[j].x,nodes[j].y);
+		}
+	}
+
+
+ctx.fillStyle="#FFF";
+fillCircle(nodes[i].x,nodes[i].y,6.5);
+ctx.fillStyle = "rgb("+nodes[i].r+","+nodes[i].g+","+nodes[i].b+")";
+fillCircle(nodes[i].x,nodes[i].y,5);
+}
+
+
+
+
+},10);
+
+socket.on('nodes',function(data){
+//console.log(data);
+
+nodes = JSON.parse(data);
+
+});
+
+/*cvs.addEventListener('mousemove', function(evt) {
+    mousePos = getMousePos(cvs, evt);
+    //console.log('Mouse position: ' + mousePos.x + ',' + mousePos.y);
+    angle = Math.atan2(mousePos.x - screenWidth/2, - (mousePos.y - screenHeight/2) )*(180/Math.PI) - 90;
+    //console.log("Angle: " + angle);
+    mv = Math.sqrt(Math.pow(mousePos.x - screenWidth/2,2)+Math.pow(mousePos.y - screenHeight/2,2));
+    //console.log(mv);
+  }, false);*/
+
+canvas.addEventListener('mouseup',function(evt) {
+	var mp = getMousePos(canvas,evt);
+	socket.emit('evt',JSON.stringify([mp.x,mp.y]));
+	console.log('clicked');
+});
+canvas.addEventListener('touchend',function(evt) {
+	evt.preventDefault();
+	var mp = evt.changedTouches;
+	for(var i = 0; i < mp.length; i++) {
+		socket.emit('evt',JSON.stringify([mp.pageX,mp.pageY]));
+	}
+	console.log('clicked');
+});
+
+
+}
---
+++ server.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('client'));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user '+ socket+' disconnected');
+  });
+  socket.on('evt',function(data) {
+	handleEvt(data,socket);
+  });
+
+});
+
+http.listen(80, function(){
+  console.log('listening on *:3000');
+});
+
+
+
+handleEvt = function(data,socket) {
+
+//console.log('recieved data: ' + data + ' from '+socket.id);
+
+try {
+data = JSON.parse(data);
+
+	console.log('got data');
+	if(data.length == 2) {
+		data[0] = parseFloat(data[0]);
+		data[1] = parseFloat(data[1]);
+
+		nodes.push(new Node(data[0],data[1],Math.random()*1 - Math.random()*1,Math.random()*1 - Math.random()*1,Math.round(Math.random()*255),Math.round(Math.random()*255),Math.round(Math.random()*255)));
+	}
+
+}
+catch(e) {
+	console.log(e);
+}
+
+};
+
+//io.emit(type, data);
+
+Global = {
+	minX:-200,
+	maxX:1280,
+	minY:-200,
+	maxY:920,
+};
+
+
+Node = function(x,y,xvel,yvel,r,g,b) {
+this.x = x;
+this.y = y;
+this.xvel = xvel;
+this.yvel = yvel;
+this.r = r;
+this.g = g;
+this.b = b;
+
+
+}
+Node.prototype.move = function() {
+	this.x += this.xvel;
+	this.y += this.yvel;
+
+	if(this.x < Global.minX || this.x > Global.maxX || this.y < Global.minY || this.y > Global.maxY) {
+		this.del = true;
+	}
+}
+
+nodes = [];
+
+nodes.push(new Node(0,0,0.1,0.1,255,74,103));
+
+setInterval(function(){
+	for(var i = 0; i < nodes.length; i++) {
+		nodes[i].move();
+	}
+	var delIndex = 0;
+	for(var i = 0; i < nodes.length - delIndex; i++) {
+		if(nodes[i].del) {
+			nodes.splice(i,1);
+			delIndex ++;
+		}
+	}
+
+
+io.emit('nodes',JSON.stringify(nodes));
+
+
+},10);
